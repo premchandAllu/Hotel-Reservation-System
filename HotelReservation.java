c70653473e5c57aa0660e73cbf72e68e4c4feabd
premchandAllu <premchandallu1999@gmail.com>
2020-10-14 10:54:26 +0530

    Added: Hotel REservation System

---
+++ HotelReservation.java
@@ -0,0 +1,9 @@
+package com.blz.HotelReservationSystem;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome to Hotel reservation System");
+	}
+}
